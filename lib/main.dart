99f119e1104b0a8ca0881b843d4d837b657fa165
abhinavmalhotra <avielric@rahabnk.com>
2025-06-20 11:01:22 +0530

    Step 6: User UI page with CRUD and offline indicator

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Offline Sync App',
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       home: BlocProvider(
